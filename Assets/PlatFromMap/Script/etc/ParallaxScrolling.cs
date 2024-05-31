da32058e243fea562c284ff4347c127edf4e1e3e
Seo-YunJeong <130148628+SeoYunJoung@users.noreply.github.com>
2024-05-31 18:30:21 +0900

    배경 추가
    background추가, 픽셀 퍼펙트, 카메라 parallax

---
+++ Assets/PlatFromMap/Script/etc/ParallaxScrolling.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    public Transform cam;
+    public float relativeMove = 0.3f;
+    public float posX = 0f;
+    public float posY = 0f;
+
+    private void Awake()
+    {
+        cam = GameObject.FindWithTag("MainCamera").transform;
+        if (gameObject.CompareTag("Background1"))
+        {
+            relativeMove = 0.84f;
+            posX = 0f;
+            posY = 3.5f;
+        } else if (gameObject.CompareTag("Background2"))
+        {
+            relativeMove = 0.9f;
+            posX = 0f;
+            posY = 2f;
+        } else if (gameObject.CompareTag("Background3"))
+        {
+            relativeMove = 0.93f;
+            posX = 0f;
+            posY = 3f;
+        } else if (gameObject.CompareTag("Background4"))
+        {
+            relativeMove = 0.95f;
+            posX = 0f;
+            posY = 0f;
+        }
+    }
+
+    private void LateUpdate() {
+        if(cam != null) {
+            transform.position = new Vector2((cam.position.x - posX) * relativeMove, (cam.position.y - posY) * relativeMove);
+        }
+        else
+        {
+            Debug.Log("배경과 메인 카메라가 연결되지 않았습니다.");
+        }
+    }
+}
