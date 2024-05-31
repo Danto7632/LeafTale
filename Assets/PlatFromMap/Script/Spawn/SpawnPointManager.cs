71533e62c5a154b74655378d9e25ecc083dd86d1
ghgh10288 <ghgh10288@naver.com>
2024-05-31 18:30:19 +0900

    5/31
    spawnPoint

--- Assets/PlatFromMap/Script/Spawn/SpawnPointManager.cs
+++ Assets/PlatFromMap/Script/Spawn/SpawnPointManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPointManager : MonoBehaviour {
+    public catMove cat;
     public int spawnCount;
 
     public void Start() {
@@ -11,6 +12,8 @@ public class SpawnPointManager : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
+            cat = other.gameObject.GetComponent<catMove>();
+
             switch(spawnCount) {
                 case 0 :
                     other.transform.position = new Vector2(-18f, 2.8f);
@@ -20,6 +23,10 @@ public class SpawnPointManager : MonoBehaviour {
                     break;
                 case 2 :
                     other.transform.position = new Vector2(59f, 11f);
+                    if(cat.isFacingRight) {
+                        newScale.x *= -1;
+                        transform.localScale = newScale;
+                    }
                     break;
             }
         }
