5bdfc0506e5ee075dc2ddec4230e83ce42083511
ghgh10288 <ghgh10288@naver.com>
2024-06-03 19:08:35 +0900

    6/3
    이동범위

--- Assets/BroomStickMap/Script/Camera/broomCamera.cs
+++ Assets/BroomStickMap/Script/Camera/broomCamera.cs
@@ -7,9 +7,20 @@ public class broomCamera : MonoBehaviour {
     public float xFixedPosition = 0f;
     public float smoothSpeed;
 
+    [Header("Movement_Limits")]
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
     void Awake() {
         smoothSpeed = 0.1f;
         Player = GameObject.FindWithTag("Player");
+
+        minX = -10f;
+        maxX = 10f;
+        minY = -2f;
+        maxY = 15f;
     }
 
     void FixedUpdate() {
@@ -17,6 +28,11 @@ public class broomCamera : MonoBehaviour {
             Vector3 desiredPosition = new Vector3(xFixedPosition, Player.transform.position.y + 3f, transform.position.z);
 
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            transform.position = smoothedPosition;
+
+            smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, minX, maxX);
+            smoothedPosition.y = Mathf.Clamp(smoothedPosition.y, minY, maxY);
 
             transform.position = smoothedPosition;
         }
