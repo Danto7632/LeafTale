224bf9b6f06f2dd9da4df831c836d31362a8f20e
ghgh10288 <ghgh10288@naver.com>
2024-07-01 19:22:59 +0900

    7/1
    리듬게임 판정 완료

---
+++ Assets/RhythmMap/Script/Check/TileCheckL.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCheckL : MonoBehaviour {
+    public bool isRock;
+    public bool isPaper;
+    public bool isScissor;
+
+    public GameObject rockPrefab;
+    public GameObject paperPrefab;
+    public GameObject scissorPrefab;
+
+    public RaycastHit2D[] isTileCheck = new RaycastHit2D[3];
+    public Vector2[] tileRayVec = new Vector2[3];
+
+    public string layerName;
+
+    public void Awake() {
+        isRock = false;
+        isPaper = false;
+        isScissor = false;
+
+        tileRayVec[0] = new Vector2(-1.5f, 1f);
+        tileRayVec[1] = new Vector2(-1.0f, 1f);
+        tileRayVec[2] = new Vector2(-0.2f, 1f);
+    }
+
+    public void getLeap(string hands) {
+        if(hands == "ROCK") {
+            isRock = true;
+            isPaper = false;
+            isScissor = false;
+            layerName = "RockTile";
+        }
+        else if(hands == "SCISSOR") {
+            isRock = false;
+            isPaper = false;
+            isScissor = true;
+            layerName = "ScissorsTile";
+        }
+        else if(hands == "PAPER") {
+            isRock = false;
+            isPaper = true;
+            isScissor = false;
+            layerName = "PaperTile";
+        }
+
+        rockPrefab.SetActive(isRock);
+        paperPrefab.SetActive(isPaper);
+        scissorPrefab.SetActive(isScissor);
+    }
+
+    void Update() {
+        for(int i = 0; i < 3; i++) {
+            Vector2 origin = tileRayVec[i];
+            Vector2 direction = Vector2.down;
+            float distance = 1f;
+
+            Debug.DrawRay(origin, direction * distance, Color.red);
+
+            isTileCheck[i] = Physics2D.Raycast(origin, direction, distance);
+            
+
+            if(isTileCheck[i].collider != null && isTileCheck[i].collider.gameObject.layer == LayerMask.NameToLayer(layerName)) {
+                switch(i) {
+                    case 0 :
+                        Debug.Log("Great");
+                        Destroy(isTileCheck[i].collider.gameObject);
+                        break;
+                    case 1 :
+                        Debug.Log("Soso");
+                        Destroy(isTileCheck[i].collider.gameObject);
+                        break;
+                }
+            }
+            else if(isTileCheck[i].collider != null && isTileCheck[i].collider.gameObject.layer != LayerMask.NameToLayer(layerName) && i == 2) {
+                Debug.Log("Fail");
+                Destroy(isTileCheck[i].collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/RhythmMap/Script/Tile/RightTileMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightTileMove : MonoBehaviour {
+    public Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        rb.velocity = new Vector2(-2f, 0);
+    }
+}
