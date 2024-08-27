c74e4ba793723acb42c46a262e3f065495077e9b
ghgh10288 <ghgh10288@naver.com>
2024-08-27 20:58:55 +0900

    8/27
    한 붓 그리기 샘플

---
+++ Assets/MagicCircleMap/Script/LinkDot.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkDot : MonoBehaviour {
+    public GameObject[] linkDots = new GameObject[2];
+
+    public bool isClicked = false;
+}
