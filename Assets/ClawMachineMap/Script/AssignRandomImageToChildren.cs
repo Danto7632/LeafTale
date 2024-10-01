38b777f8f9d88758122445b7aea02d781fa4be66
ghgh10288 <ghgh10288@naver.com>
2024-10-01 21:34:06 +0900

    10/1
    게이지바 개선

--- Assets/ClawMachineMap/Script/AssignRandomImageToChildren.cs
+++ Assets/ClawMachineMap/Script/AssignRandomImageToChildren.cs
@@ -3,29 +3,22 @@ using UnityEngine.UI;
 
 public class AssignRandomImageToChildren : MonoBehaviour {
     public Sprite[] images; // 9개의 이미지를 배열로 저장
+    public Sprite[] pointImages;
+
+    public Image one;
+    public Image two;
+
+    public int randomIndex;
 
     private void Start() {
-        if (images.Length != 9) {
-            Debug.LogError("9개의 이미지를 할당해주세요!");
-            return;
-        }
-
         AssignSameRandomImageToChildren();
     }
 
     private void AssignSameRandomImageToChildren() {
         // 9개의 이미지 중 랜덤으로 하나 선택
-        int randomIndex = Random.Range(0, images.Length);
-        Sprite selectedImage = images[randomIndex];
+        randomIndex = Random.Range(0, images.Length);
 
-        // 자식 오브젝트들을 순회
-        foreach (Transform child in transform) {
-            Image childImage = child.GetComponent<Image>();
-
-            if (childImage != null) {
-                // 선택된 이미지를 자식에게 할당
-                childImage.sprite = selectedImage;
-            }
-        }
+        one.sprite = images[randomIndex];
+        two.sprite = pointImages[randomIndex];
     }
 }
