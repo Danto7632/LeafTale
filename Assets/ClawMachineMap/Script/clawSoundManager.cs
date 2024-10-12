69b61503c76915dc55938d10b1011ed635b7620b
Danto <rlarladlstjd@gmail.com>
2024-10-13 02:16:41 +0900

    clawbgm

--- Assets/ClawMachineMap/Script/clawSoundManager.cs
+++ Assets/ClawMachineMap/Script/clawSoundManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class clawSoundManager : MonoBehaviour {
+
+    public AudioSource clawbgm;
     public AudioSource clawSound;
     public AudioSource itemGoodSound;
     public AudioSource itemFailSound;
