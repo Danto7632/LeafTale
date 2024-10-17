a5b8141f8a5888a2733d5e210ae2d147339598ef
Danto <rlarladlstjd@gmail.com>
2024-10-17 21:23:30 +0900

    thanos

--- Assets/RhythmMap/Script/DieBehavior.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DieBehavior : StateMachineBehaviour {
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        animator.gameObject.SetActive(false);
-    }
-}
